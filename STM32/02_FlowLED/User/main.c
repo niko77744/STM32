9bccd36790f849838b76bbdf9a9b5826607ad429
niko77744 <1836045035@qq.com>
2024-09-08 16:20:04 +0800

    test

--- STM32/02_FlowLED/User/main.c
+++ STM32/02_FlowLED/User/main.c
@@ -3,22 +3,21 @@
 #include "Delay.h"
 // | ´ò¿ª   &~ ¹Ø±Õ
 
-
 int main() {
-    uint16_t temp[] = { LED1,LED2,LED3 };
+    uint16_t leds[] = { LED1,LED2,LED3 };
 
     Driver_LED_Init();
 
-    // Driver_LED_OnAll(temp, 3);
+    // Driver_LED_OnAll(leds, 3);
     // Delay_ms(2000);
-    // Driver_LED_OffAll(temp, 3);
+    // Driver_LED_OffAll(leds, 3);
 
     while (1) {
         for (uint8_t i = 0; i < 3; i++)
         {
-            Driver_LED_On(temp[i]);
+            Driver_LED_On(leds[i]);
             Delay_ms(1000);
-            Driver_LED_Off(temp[i]);
+            Driver_LED_Off(leds[i]);
         }
     }
 }
