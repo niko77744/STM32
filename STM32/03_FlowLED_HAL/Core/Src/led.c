db26dc7fdfff9017f4f78f0db8f8f129da98f2ca
niko77744 <1836045035@qq.com>
2024-09-09 18:40:41 +0800

    优化LED流水灯逻辑，实现更直观

--- STM32/03_FlowLED_HAL/Core/Src/led.c
+++ STM32/03_FlowLED_HAL/Core/Src/led.c
@@ -29,24 +29,43 @@ void LED_OffAll(uint16_t leds[], uint8_t len) {
 }
 
 
+/**
+ * @brief 实现LED流水灯效果
+ * 
+ * 通过依次点亮和熄灭LED灯，实现流水灯的效果。该函数按顺序点亮所有的LED灯，
+ * 然后逆序熄灭它们。使用了两个for循环来分别处理点亮和熄灭的过程。
+ * 
+ * @param leds 一个包含所有LED灯的数组，数组中的每个元素代表一个LED灯
+ * @param len 表示数组中LED灯的数量
+ */
 void LED_FlowLight(uint16_t leds[], uint8_t len) {
-    static  uint8_t i = 0;
-    static bool flag = 0;
+    // 顺次点亮LED灯
+    for (uint8_t i = 0; i < len; i++) {
+        LED_Toogle(leds[i]); // 切换LED灯状态
+        HAL_Delay(1000); // 延时1秒
+    }
+    
+    // 逆序熄灭LED灯
+    for (int8_t i = (len - 1); i >= 0; i--) {
+        LED_Toogle(leds[i]); // 切换LED灯状态
+        HAL_Delay(1000); // 延时1秒
+    }
 
-    if (0 == i)
-    {
-        flag = 1;
-        LED_Toogle(leds[i]);
-        HAL_Delay(1000);
-    }
-    else if ((len - 1) == i)
-    {
-        flag = 0;
-        LED_Toogle(leds[i]);
-        HAL_Delay(1000);
-    }
-    (flag == 1) ? (i++) : (i--);
-    LED_Toogle(leds[i]);
-    HAL_Delay(1000);
+    // 以下是尝试使用单循环实现相同功能的代码，但被注释掉了
+    // static uint8_t i = 0; // 用于记录当前点亮/熄灭的LED灯索引
+    // static bool flag = 0; // 用于控制是正向点亮还是反向熄灭
+
+    // if (0 == i) {
+    //     flag = 1; // 设置方向标志为正向
+    //     LED_Toogle(leds[i]); // 切换第一个LED灯状态
+    //     HAL_Delay(1000); // 延时1秒
+    // } else if ((len - 1) == i) {
+    //     flag = 0; // 设置方向标志为反向
+    //     LED_Toogle(leds[i]); // 切换最后一个LED灯状态
+    //     HAL_Delay(1000); // 延时1秒
+    // }
+    // (flag == 1) ? (i++) : (i--); // 根据方向标志改变LED灯索引
+    // LED_Toogle(leds[i]); // 切换当前LED灯状态
+    // HAL_Delay(1000); // 延时1秒
 }
 
