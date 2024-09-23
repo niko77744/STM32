48a66fe79bad3c14557476caf9bab415904cfd83
niko77744 <1836045035@qq.com>
2024-09-23 10:05:52 +0800

    开发板时钟驱动初始化代码已更新，以支持新的时钟配置。此次更改涉及多个与定时器和GPIO相关的函数，包括但不限于TIM1、TIM4、TIM5和TIM6的初始化。中断处理函数也进行了调整，以确保正确处理时钟事件。整体目的是精简时钟初始化流程，确保所有时钟配置均符合最新的系统需求。

--- STM32/21_Advanced_Timer/User/main.c
+++ STM32/21_Advanced_Timer/User/main.c
@@ -1,29 +1,32 @@
-#include "Com_Delay.h"
-#include "Com_SysTick.h"
-#include "Driver_IIC_Hard.h"
-#include "Driver_IIC_Soft.h"
 #include "Driver_Key.h"
 #include "Driver_LED.h"
+
 #include "Driver_USART.h"
+#include "Driver_IIC_Hard.h"
+#include "Driver_IIC_Soft.h"
+
+#include "Com_Delay.h"
+#include "Com_SysTick.h"
 #include "Driver_BaseTimer.h"
 #include "Driver_GeneralTimer.h"
+#include "Driver_AdvancedTimer.h"
+
 #include "Inf_EEPROM.h"
 
 uint8_t buffer[100] = { 0 };
 uint8_t len = 0;
 extern uint8_t USART_BRF;
 
-uint8_t LED1_DutyCycle = 0;
-uint8_t LED2_DutyCycle = 99;
-uint8_t LED1_flag = 1;
-uint8_t LED2_flag = 1;
+// uint8_t LED1_DutyCycle = 0;
+// uint8_t LED2_DutyCycle = 99;
+// uint8_t LED1_flag = 1;
+// uint8_t LED2_flag = 1;
 
 int main(int argc, char const* argv[])
 {
     Driver_USART_Init();
-    Driver_GeneralTimer4_Init();
-    Delay_ms(10);
-    Driver_GeneralTimer5_Init();
+    Driver_AdvancedTimer1_Init();
+    Driver_KEY_Init();
 
     while (1) {
         // if (USART_BRF)
@@ -54,10 +57,9 @@ int main(int argc, char const* argv[])
         // Driver_GeneralTimer5_LED1_SetDutyCycle(LED1_DutyCycle);
         // Driver_GeneralTimer5_LED2_SetDutyCycle(LED2_DutyCycle);
 
+        // printf("T = %.2f(ms),  F=%.2f(Hz/s)  , D = %.2f %%\n", Driver_GeneralTimer4_GetCycle(), Driver_GeneralTimer4_GetFrequency(), (Driver_GeneralTimer4_GetDutyCycle() * 100.0));
 
-        printf("T = %.2f(ms),  F=%.2f(Hz/s)  , D = %.2f %%\n", Driver_GeneralTimer4_GetCycle(), Driver_GeneralTimer4_GetFrequency(), (Driver_GeneralTimer4_GetDutyCycle() * 100.0));
-
-        Delay_ms(1000);
+        // Delay_ms(1000);
     }
 }
 
